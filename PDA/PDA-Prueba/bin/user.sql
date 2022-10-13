80912fbaaf8172e07aa598deb1817d15c4c80e43
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-10-12 22:12:38 -0300

    Cambios en librerias

---
+++ PDA/PDA-Prueba/bin/user.sql
@@ -0,0 +1,11 @@
+alter session set "_ORACLE_SCRIPT"=true;
+
+CREATE USER miniproyecto
+IDENTIFIED BY 1234
+DEFAULT TABLESPACE USERS;
+
+GRANT 
+CREATE SESSION, RESOURCE, SELECT_CATALOG_ROLE
+TO miniproyecto;
+
+ALTER USER miniproyecto quota unlimited on USERS;
